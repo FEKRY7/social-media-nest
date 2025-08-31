13e94f789b8e0ecf40ef3d9ef0dbe01731641b9e
FEKRY7 <159729804+FEKRY7@users.noreply.github.com>
2025-08-31 23:48:50 +0300

    Update create-token.dto.ts

--- src/Token/dtos/create-token.dto.ts
+++ src/Token/dtos/create-token.dto.ts
@@ -1,13 +1,1 @@
-import { IsString, IsNumber, IsNotEmpty, Min, MinLength, MaxLength, Max, Length } from 'class-validator';
 
-export class CreateReviewDto {
-    @IsNumber() 
-    @Min(1)
-    @Max(5)
-    rating: number
-    
-    @IsString() 
-    @MinLength(2)
-    comment: string;
-
-}
